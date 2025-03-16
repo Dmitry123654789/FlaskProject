f870b909e4e331c33a1a8189474e96c747b19995
Dmitry <pokemon2708@yandex.ru>
2025-03-16 22:52:43 +0300

    Добавлена иконка и частично добавлен nav bar

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def home_page():
+    return render_template('home.html')
+
+
 if __name__ == '__main__':
-    app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+    # app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
     app.run(port=8080, host='127.0.0.1')
