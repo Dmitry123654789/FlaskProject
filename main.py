dbe57b998ff0f7735037de8e0b970c1bc3e955f0
Dmitry <pokemon2708@yandex.ru>
2025-03-22 23:15:26 +0300

    Главная страница расширена

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home_page():
-    return render_template('base.html')
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
